a45ef4e84e3db6f3edd1ddc5dfae93f76ad28428
Loan Tricot <ltricot@gmail.com>
2019-05-20 13:09:55 +0200

    :)

--- manager/manage.py
+++ manager/manage.py
@@ -1,16 +1,24 @@
 from urllib.request import urlopen
 import zipfile, os, shutil
 
+import click
+
 
 SOURCE = 'https://zenodo.org/record/1204408/files/savmap_dataset_v2.zip?download=1' 
 IMAGES, META, DATA = 'images', 'meta', 'data.zip'
-def dldata(source):
+
+@click.group()
+def cli():
+	...
+
+@cli.command()
+def dldata():
 	# already downloaded
 	if os.path.exists(DATA):
 		print('already downloaded')
 		return
 
-	response = urlopen(source)
+	response = urlopen(SOURCE)
 	bindata = response.read()
 	
 	with open(DATA, 'wb') as a:
@@ -23,13 +31,10 @@ def dldata(source):
 		for name in z.namelist():
 			# extract images & meta to respective directories
 			_dir = IMAGES if name.endswith('JPG') else META
-			z.extract(name, path=os.path.join(_dir, name))
+			z.extract(name, path=_dir)
 
+@cli.command()
 def clean():
 	shutil.rmtree(IMAGES)
 	shutil.rmtree(META)
 	os.remove(DATA)
-
-
-if __name__ == '__main__':
-	dldata(SOURCE)
