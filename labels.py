86de3c20eb08d51306a2733b0be5489a04944195
SebastienLabbe <sebazore.bob@gmail.com>
2019-06-04 16:12:00 +0200

    labels done

--- labels.py
+++ labels.py
@@ -1,43 +1,43 @@
 import os
 import pygeoj
-from PIL import Image
 import numpy as np
-import pickle 
+import json 
 from collections import namedtuple
+from jpegtran import JPEGImage
+
 
 Bbox = namedtuple('Bbox', 'x y w h c')
-
-S = 7
-B = 2
-C = 1
-
-size = (4000,3000)
 
 IMAGES = 'images/'
 META = 'meta/'
 
+LABELS = 'labels.json'
 
 def get_labels():
-	try:
-		with open('labels.txt','rb') as file:
-			label = pickle.load(file)
-	except:
-		label = {}
-		#image_names = os.listdir(IMAGES)
-		for file_name in os.listdir(META):
-			if file_name.endswith('.geojson'):
-				file = pygeoj.load(META+file_name)
-				for feature in file:
-					x,y,xt,yt = feature.geometry.bbox
-					w,h = (xt-x,yt-y)
-					c = 1
-					bbox = Bbox(x/size[0],y/size[1],w/size[0],h/size[0],c)
-					if(feature.properties['IMAGEUUID'] in label):
-						label[feature.properties['IMAGEUUID']].append(l)
-					else:
-						label[feature.properties['IMAGEUUID']] = [l]
-		with open('labels.txt','wb') as file:
-			pickle.dump(label,file)
+	if os.path.exists(LABELS):
+		with open(LABELS) as file:
+			label = json.load(file)
+		return label
+	label = {}
+	#image_names = os.listdir(IMAGES)
+	for file_name in os.listdir(META):
+		if file_name.endswith('.geojson'):
+			file = pygeoj.load(META+file_name)
+			for feature in file:
+				print(feature.properties['TAGUUID'])
+				img = JPEGImage(IMAGES+feature.properties['IMAGEUUID']+'.JPG')
+				x1,y1,x2,y2 = feature.geometry.bbox
+				w,h = (x2-x1,y2-y1)
+				x,y = ((x1+x2)/2,(y1+y2)/2)
+				c = 1
+				bbox = Bbox(x/img.width,y/img.height,w/img.width,h/img.height,c)
+				if(feature.properties['IMAGEUUID'] in label):
+					label[feature.properties['IMAGEUUID']].append(bbox)
+				else:
+					label[feature.properties['IMAGEUUID']] = [bbox]
+	
+	with open(LABELS) as file:
+		json.dump(label,file)
 	return label
 
 if __name__ == '__main__':
