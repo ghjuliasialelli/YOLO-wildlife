584671ffaae2feb6eaa33a9186ef70343305f71f
SebastienLabbe <sebazore.bob@gmail.com>
2019-06-04 20:00:57 +0200

    model

--- network/archi.py
+++ network/archi.py
@@ -9,13 +9,19 @@ from typing import Iterable, Tuple
 from augment import datagen, Imgs, Labels
 
 structure = [
-		[3,64,3],
+		[
+		[3,64,3], #Conv2d layers (input,output,kernel size)
+		[64,64,3],
+		[64,64,3],
 		[64,64,3],
 		[64,64,3],
 		[64,64,3],
 		[64,64,3],
 		[64,3,3],
+		],
+		[2,4,6] #layers at which to do a Maxpool2d(of 2) pooling
 		]
+
 
 class YOLO(nn.Module):
 	def __init__(self):
@@ -24,20 +30,19 @@ class YOLO(nn.Module):
 		'''        
 		super(YOLO, self).__init__()
 		self.convs = []
-		for x in structure:
+		for x in structure[0]:
 			self.convs.append(nn.Conv2d(x[0],x[1],x[2],padding=(x[2]-1)//2))
 		self.pool = nn.MaxPool2d(2)
+		self.pools = structure[1]
 	
 	def forward(self,x):
 		'''compute output of (batch size) * S * S * 3
 		'''
-		x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+		for i,conv in enumerate(self.convs):
+			if i in self.pools:
+				x = self.pool(x)
+			x = F.relu(conv(x))
+		return x
 
 def train(yolo: YOLO, data: Iterable[Tuple[Imgs, Labels]], lr: float, epochs: int):
 	'''given an initialized yolo object, train
