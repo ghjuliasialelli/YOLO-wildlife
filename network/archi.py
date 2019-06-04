179db856733356c266272b517c961bbd6c2c238a
Loan Tricot <ltricot@gmail.com>
2019-06-04 20:03:13 +0200

    pre merge

--- network/archi.py
+++ network/archi.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.optim as optim
 import torch.nn.functional as F
 
 from collections import namedtuple
@@ -18,13 +19,25 @@ class YOLO:
 		'''
 		...
 
-def train(yolo: YOLO, data: Iterable[Tuple[Imgs, Labels]], lr: float, epochs: int):
+def train(yolo: YOLO, data: Iterable[Tuple[Imgs, Labels]], lr: float, steps: int):
 	'''given an initialized yolo object, train
 	'''
-	...
+	rloss = 0
 
+	lossf = nn.MSELoss()
+	opt = optim.SGD(yolo.parameters(), lr=lr, momentum=0.9)
+	for i, (imgs, lbls) in enumerate(data):
+		if i >= steps:
+			break
 
+		# optimization step: compute loss and
+		# backpropagate
+		opt.zero_grad()
+		pred = yolo(imgs)
+		loss = lossf(pred, lbls)
+		loss.backward()
+		opt.step()
 
-def datagen():
-	while True:
-		yield imgs, labels
+		# expectation over loss, with recent loss more
+		# important
+		rloss = .9 * rloss + .1 * loss.item()
